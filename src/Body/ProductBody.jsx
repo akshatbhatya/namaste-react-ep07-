4e975cef53aa5c08d6a357c50db90ebb2a0d4ee4
akshat <akshatbhatya@gmail.com>
2024-01-20 23:18:30 +0530

    add card data

--- src/Body/ProductBody.jsx
+++ src/Body/ProductBody.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import "./ProductBody.css"
 import Card from '../Card/Card'
 import { swiggy_url } from '../utils/constants'
@@ -8,12 +8,23 @@ import { swiggy_url } from '../utils/constants'
 
 function ProductBody() {
 
+    const [product,setProduct]=useState([])
+
 
     useEffect(() => {
         let fetchData = async () => {
             let data = await fetch(swiggy_url);
             let response = await data.json()
-            console.log(response.data.cards[4].card.card.gridElements.infoWithStyle.restaurants);
+            let sortedData=(response.data.cards[4].card.card.gridElements.infoWithStyle.restaurants);
+            console.log(sortedData);
+
+            let allProduct=[]
+
+            for(let i=0;i<sortedData.length;i++){
+                allProduct.push(sortedData[i].info)
+
+            }
+            setProduct(allProduct);
 
         }
         fetchData()
@@ -21,7 +32,11 @@ function ProductBody() {
     return (
         <div className='productbody-parent'>
 
-            <Card />
+            {
+                product.map((singleProduct)=>{
+                   return <Card data={singleProduct} key={singleProduct.id}/>
+                })
+            }
         </div>
     )
 }
--- src/Card/Card.jsx
+++ src/Card/Card.jsx
@@ -1,17 +1,23 @@
 import React from 'react'
 import "./Card.css"
+import { image_link } from '../utils/constants'
+            
 
-function Card() {
+function Card({data}) {
+  const{name,cloudinaryImageId,locality,areaName,costForTwo,cuisines,avgRating,sla}=data;
   return (
     <div className='card_parent'>
         <div className="card_img">
-            <img src="" alt="" />
+            <img src={image_link+cloudinaryImageId} alt={image_link} />
         </div>
         <div className="content">
-            <h4></h4>
-            <h4></h4>
-            <h4></h4>
-            <h4></h4>
+            <h4>{name}</h4>
+            <h4>{areaName}</h4>
+            <h4>{locality}</h4>
+            <h4>{cuisines}</h4>
+            <h4>{costForTwo}</h4>
+            <h4>{avgRating}</h4>
+            <h4>{sla.deliveryTime}</h4>
         </div>
       
     </div>
