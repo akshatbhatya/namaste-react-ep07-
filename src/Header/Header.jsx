8487b9d3be6931fe7b041a10eafbd114ac069f82
akshat <akshatbhatya@gmail.com>
2024-01-21 01:59:59 +0530

    add link

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import "./Header.css"
+import { Link } from 'react-router-dom'
 function Header() {
     return (
         <div className='header_parent'>
@@ -9,9 +10,9 @@ function Header() {
             </div>
             <div className="navbar">
                 <ul>
-                    <li>Home</li>
-                    <li>About Us</li>
-                    <li>Contact Us</li>
+                    <li><Link to={"/"}>Home</Link></li>
+                    <li><Link to="/about">About Us</Link></li>
+                    <li><Link to={"/contact"}>Contact Us</Link></li>
                     <li>Cart</li>
                 </ul>
             </div>
