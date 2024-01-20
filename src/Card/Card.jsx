b138736a332b3992226ea31fe497734e3982181f
akshat <akshatbhatya@gmail.com>
2024-01-20 22:43:58 +0530

    add card

---
+++ src/Card/Card.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./Card.css"
+
+function Card() {
+  return (
+    <div className='card_parent'>
+        <div className="card_img">
+            <img src="" alt="" />
+        </div>
+        <div className="content">
+            <h4></h4>
+            <h4></h4>
+            <h4></h4>
+            <h4></h4>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Card
