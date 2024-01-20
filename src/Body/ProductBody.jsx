e1e7f8cce192cee8d64c33226e6cce15143b2386
akshat <akshatbhatya@gmail.com>
2024-01-20 22:41:01 +0530

    addbody component

---
+++ src/Body/ProductBody.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./ProductBody.css"
+
+function ProductBody() {
+  return (
+    <div className='productbody-parent'>
+      
+    </div>
+  )
+}
+
+export default ProductBody
