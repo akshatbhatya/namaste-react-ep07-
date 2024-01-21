4dff3d6987edb382df17e9d81bc55793f1d5ad27
akshat <akshatbhatya@gmail.com>
2024-01-21 11:52:21 +0530

    add api

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,6 @@
 export let  swiggy_url="https://www.swiggy.com/dapi/restaurants/list/v5?lat=31.3260152&lng=75.57618289999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
 
 export let image_link="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_360/";
+
+
+export let CardDetail="https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=31.3260152&lng=75.57618289999999&restaurantId=248278&catalog_qa=undefined&submitAction=ENTER";
