94df924c66b27dfe6d1fdeb11c8a0b17e674f14a
akshat <akshatbhatya@gmail.com>
2024-01-21 16:59:52 +0530

    add data to page

--- src/ReasturantPage/ReasturantPage.jsx
+++ src/ReasturantPage/ReasturantPage.jsx
@@ -1,26 +1,41 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
 
 
 function ReasturantPage() {
 
-    let {resId} = useParams()
+    let [product, setProduct] = useState([])
+
+    let { resId } = useParams()
 
     console.log(resId);
     useEffect(() => {
 
-        
+
         fetchData()
     }, [])
 
-    let fetchData=async()=>{
-        let data=await fetch(`https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=31.3260152&lng=75.57618289999999&restaurantId=${resId}&catalog_qa=undefined&submitAction=ENTER`)
-        let response=await data.json()
-        console.log(response);
+    let fetchData = async () => {
+        let data = await fetch(`https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=31.3260152&lng=75.57618289999999&restaurantId=${resId}&catalog_qa=undefined&submitAction=ENTER`)
+        let response = await data.json()
+        let sortedOne = (response.data.cards[0].card.card.info);
+        setProduct(sortedOne)
+
     }
+    console.log(product);
+
+
+
     return (
-        <div>
-            ReasturantPage
+        <div className='resturant'>
+            <h4>{product.name}</h4>
+            <h4>{product.areaName}</h4>
+            <h4>{product.city}</h4>
+            <h4>{product.costForTwoMessage}</h4>
+            <h4>{product.cuisines}</h4>
+
+
+
         </div>
     )
 }
